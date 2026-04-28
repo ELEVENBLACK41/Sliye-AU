8aa10a7c2d28b361f05ee984444cab59fcde8eea
shaoliye <shaoliye@fengmap.com>
2026-04-28 17:24:48 +0800

    Sliye 链接数据库了 初始化prisma，详见md与官方文档

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,7 +2,7 @@
  * @Author: shaoliye
  * @Date: 2026-04-24 13:43:49
  * @Email: shaoliye@fengmap.com
- * @LastEditTime: 2026-04-24 15:37:37
+ * @LastEditTime: 2026-04-28 16:40:33
  * @LastEditors: shaoliye
  * @LastEditorsEmail: shaoliye@fengmap.com
  * @Description:
@@ -10,11 +10,12 @@
  */
 import { Module } from '@nestjs/common';
 import { TestModule } from './modules/test/test.module';
+import { ConfigModule } from '@nestjs/config';
 // import { AppController } from './app.controller';
 // import { AppService } from './app.service';
 
 @Module({
-  imports: [TestModule],
+  imports: [TestModule, ConfigModule.forRoot()],
   // controllers: [AppController],
   // providers: [AppService],
 })
