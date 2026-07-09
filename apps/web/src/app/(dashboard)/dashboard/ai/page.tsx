669aff031f6e26f1a5592d8b5c5ec6f79eae3cc4
shaoliye <elevenblack41@gmail.com>
2026-07-09 17:06:47 +0800

    增加天气工具调用

--- apps/web/src/app/(dashboard)/dashboard/ai/page.tsx
+++ apps/web/src/app/(dashboard)/dashboard/ai/page.tsx
@@ -2,10 +2,10 @@
  * @Author: shaoliye
  * @Date: 2026-07-06 15:33:12
  * @Email: elevenblack41@gmail.com
- * @LastEditTime: 2026-07-09 15:08:15
+ * @LastEditTime: 2026-07-09 16:31:58
  * @LastEditors: shaoliye elevenblack41@gmail.com
  * @LastEditorsEmail: elevenblack41@gmail.com
- * @Description: 
+ * @Description:
  * @Copyright: Copyright 1990 - 2026
  */
 'use client';
@@ -15,23 +15,27 @@ import { useState } from 'react';
 
 export default function Chat() {
   const [input, setInput] = useState('');
+  //vercek 的 AI SDK 提供的 useChat hook，封装了与后端 API 的交互逻辑，messages,当前聊天消息（一个包含id、role和parts属性的对象数组）,sendMessage- 向聊天 API 发送消息的函数。
   const { messages, sendMessage } = useChat();
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      {messages.map(message => (
+      {messages.map((message) => (
         <div key={message.id} className="whitespace-pre-wrap">
           {message.role === 'user' ? 'User: ' : 'AI: '}
           {message.parts.map((part, i) => {
             switch (part.type) {
               case 'text':
                 return <div key={`${message.id}-${i}`}>{part.text}</div>;
+              case 'tool-weather':
+              case 'tool-convertFahrenheitToCelsius':
+                return <pre key={`${message.id}-${i}`}>{JSON.stringify(part, null, 2)}</pre>;
             }
           })}
         </div>
       ))}
 
       <form
-        onSubmit={e => {
+        onSubmit={(e) => {
           e.preventDefault();
           sendMessage({ text: input });
           setInput('');
@@ -41,7 +45,7 @@ export default function Chat() {
           className="fixed dark:bg-zinc-900 bottom-0 w-full max-w-md p-2 mb-8 border border-zinc-300 dark:border-zinc-800 rounded shadow-xl"
           value={input}
           placeholder="Say something..."
-          onChange={e => setInput(e.currentTarget.value)}
+          onChange={(e) => setInput(e.currentTarget.value)}
         />
       </form>
     </div>
--- apps/web/src/app/api/chat/route.ts
+++ apps/web/src/app/api/chat/route.ts
@@ -2,7 +2,7 @@
  * @Author: shaoliye elevenblack41@gmail.com
  * @Date: 2026-07-09 14:57:15
  * @LastEditors: shaoliye elevenblack41@gmail.com
- * @LastEditTime: 2026-07-09 16:14:22
+ * @LastEditTime: 2026-07-09 16:39:11
  * @FilePath: \NextNest\apps\web\src\app\api\chat\route.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -15,16 +15,50 @@ import {
   streamText,
   UIMessage,
   convertToModelMessages,
+  tool,
+  isStepCount,
   createUIMessageStreamResponse,
   toUIMessageStream,
 } from 'ai';
+import { z } from 'zod';
 
 export async function POST(req: Request) {
   const { messages }: { messages: UIMessage[] } = await req.json();
 
   const result = streamText({
-    model: "openai/gpt-4.1",
+    model: 'openai/gpt-4.1',
     messages: await convertToModelMessages(messages),
+    stopWhen: isStepCount(5),
+    tools: {
+      weather: tool({
+        description: 'Get the weather in a location (fahrenheit)',
+        inputSchema: z.object({
+          location: z.string().describe('The location to get the weather for'),
+        }),
+        execute: async ({ location }) => {
+          const temperature = Math.round(Math.random() * (90 - 32) + 32);
+          return {
+            location,
+            temperature,
+          };
+        },
+      }),
+      convertFahrenheitToCelsius: tool({
+        description: 'Convert a temperature in fahrenheit to celsius',
+        inputSchema: z.object({
+          temperature: z.number().describe('The temperature in fahrenheit to convert'),
+        }),
+        execute: async ({ temperature }) => {
+          const celsius = Math.round((temperature - 32) * (5 / 9));
+          return {
+            celsius,
+          };
+        },
+      }),
+    },
+    onStepEnd: ({ toolResults }) => {
+      console.log('记录使用工具的步骤----------------------', toolResults);
+    },
   });
 
   return createUIMessageStreamResponse({
